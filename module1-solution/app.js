1c511623577e0f9b71b705d56c40ec810e648d8c
Gonzalo.Ailan <Gonzalo.Ailan@almundo.com>
2017-08-24 20:21:01 -0300

    feat: adds initial release

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,13 +1,40 @@
 'use strict';
 
 (() => {
-	angular.module('angularApp', [])
+	angular.module('LunchCheck', [])
 
-	.controller('mainCtrl', mainCtrl);
+	.controller('LunchCheckController', LunchCheckController);
 
-	mainCtrl.$inject = ['$scope'];
+	LunchCheckController.$inject = ['$scope'];
 
-	function mainCtrl($scope) {
+	function LunchCheckController($scope) {
+
+		$scope.foodString = "";
+
+		$scope.checkFood = (stringList) => {
+			const MAX_NUMBER = 3;
+			const FOOD_AMOUNT = numberFromStringList(stringList);
+			
+			if(!$scope.foodString) {
+				$scope.message = "Please enter data first";
+				$scope.textAreaClass = "input-error";
+			} else if(FOOD_AMOUNT > MAX_NUMBER) {
+				$scope.message = "Too much!";
+				$scope.textAreaClass = "input-confirmation";
+
+			} else {
+				$scope.message = "Enjoy!";
+				$scope.textAreaClass = "input-confirmation";
+
+			}
+			$scope.messageDisplay = true;
+			
+		}
+
+		function numberFromStringList(stringList) {
+			const arrayFromStringList = stringList.split(',');
+			return arrayFromStringList.length;
+		}
 		
 	};
 
